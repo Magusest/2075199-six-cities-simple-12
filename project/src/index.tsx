02370cbe4346fc2e0395019c16f2e68b2a7bece6
Magusest <redikor49@gmail.com>
2023-03-03 12:25:54 +0300

    Пробросил пропсы к MainPage

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+
+const placesToStayCount = 123;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesToStay = {placesToStayCount}
+    />
   </React.StrictMode>,
 );
