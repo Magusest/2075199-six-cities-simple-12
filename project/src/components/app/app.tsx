5e970d4f0846b0ff8b8b4c70f2526c711a71ac91
Magusest <redikor49@gmail.com>
2023-03-04 18:57:43 +0300

    Добавил роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,10 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 
 type AppScreenProps = {
@@ -8,7 +13,14 @@ type AppScreenProps = {
 
 function App({placesToStay}: AppScreenProps): JSX.Element {
   return (
-    <MainPage placesToStay={placesToStay} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage placesToStay={placesToStay} />}/>
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path ={AppRoute.Room} element={<OfferPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
