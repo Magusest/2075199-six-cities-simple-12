9c4893b782cf6a9138f9902292585799e35ceabd
Magusest <redikor49@gmail.com>
2023-03-18 17:12:03 +0300

    Delete HeadetForLogin path

--- project/src/components/index.ts
+++ project/src/components/index.ts
@@ -2,7 +2,6 @@ export { default as App } from './app/app';
 export { default as Logo } from './logo/logo';
 export { default as PlaceCard } from './place-card/place-card';
 export { default as Header } from './header/header';
-export { default as HeaderForAuth } from './header/header-for-auth';
 export { default as OffersList } from './offers-list/offers-list';
 export { default as ReviewsForm } from './reviews-form/reviews-form';
 export { default as Authtorization } from './authorization/authorization';
